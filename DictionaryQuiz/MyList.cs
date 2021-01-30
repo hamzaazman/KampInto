a8449ae7bee9d02a5f37cae9e52a19005994feab
hamzaazman <hamzaazman6776@gmail.com>
2021-01-30 19:09:36 +0300

    Dictionary collection created

---
+++ DictionaryQuiz/MyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryQuiz
+{
+    class MyList<T1,T2>
+    {
+        T1[] IdentifyNumber;
+        T2[] name;
+
+        public MyList()
+        {
+            IdentifyNumber = new T1[0];
+            name = new T2[0];
+        }
+        public void Add(T1 Number, T2 Name)
+        {
+            T1[] tempNumber = IdentifyNumber;
+            T2[] tempName = name;
+            IdentifyNumber = new T1[IdentifyNumber.Length + 1];
+            name = new T2[name.Length + 1];
+
+            for (int i = 0; i < tempNumber.Length; i++)
+            {
+                IdentifyNumber[i] = tempNumber[i]; 
+            }
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+            }
+
+            IdentifyNumber[IdentifyNumber.Length - 1] = Number;
+            name[name.Length - 1] = Name;
+
+            Console.WriteLine(Number + " Numaralı " + Name + " Adlı kişi eklendi");
+
+        }
+
+    }
+}
---
+++ DictionaryQuiz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictionaryQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> user = new MyList<int, string>();
+            user.Add(6767,"Hamza AZMAN");
+            user.Add(3434,"Ali Fatih AZMAN");
+            
+
+
+        }
+    }
+}
