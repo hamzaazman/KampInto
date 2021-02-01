0706505080b00bc88e69da816e3e6f0788f3b89f
hamzaazman <hamzaazman6776@gmail.com>
2021-02-01 18:57:30 +0300

    Deleted linq project

---
+++ LinqProject/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LinqProject
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantitiyPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+    }
+}
---
+++ LinqProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId = 1, CategoryName= "Bilgisayar"},
+                new Category{CategoryId = 2, CategoryName= "Telefon"},
+            };
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId = 1, CategoryId=1,ProductName ="Acer Laptop",QuantitiyPerUnit="32 GB Ram", UnitInStock=5,UnitPrice=10000},
+                new Product{ProductId = 2, CategoryId=1,ProductName ="Lenova Laptop",QuantitiyPerUnit="16 GB Ram", UnitInStock=2,UnitPrice=8000},
+                new Product{ProductId = 3, CategoryId=2,ProductName ="Samsung Telefon",QuantitiyPerUnit="4 GB Ram", UnitInStock=5,UnitPrice=3500}
+            };
+            Console.WriteLine("Linq------------");
+           
+        }
+     
+    }
+}
